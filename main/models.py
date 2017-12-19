ec593398e88b4cceaf165a255c3fe5f36e8c0740
Brayan Tabares <brayantabares10a2014@gmail.com>
2017-12-19 14:56:43 -0500

    a

--- main/models.py
+++ main/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-"""class Registrado(models.Model):
+class Registrado(models.Model):
 	nombre = models.CharField(max_length=100,blank=True,null=True)
 	email=models.EmailField()
 	timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
 	def __str__(self):
 		return self.email
-"""
+
 print("adwadsefseg")
