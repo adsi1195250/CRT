07099e5f1023e9ea98913a811ec491d75c8953df
root <bryan12gh@gmail.com>
2017-12-19 12:52:33 -0500

    "try"

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Registrado(models.Model):
-	nombre = models.CharField(max_length=111,blank=True,null=True)
+	nombre = models.CharField(max_length=121,blank=True,null=True)
 	email=models.EmailField()
 	timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
 	def __str__(self):
