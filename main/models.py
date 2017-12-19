041d452be3bddcf1f15491b727c9c4b02ed50421
Brayan Tabares <brayantabares10a2014@gmail.com>
2017-12-19 15:19:51 -0500

    Ultimo cambio

--- main/models.py
+++ main/models.py
@@ -8,13 +8,4 @@ class Registrado(models.Model):
 	email=models.EmailField()
 	timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
 	def __str__(self):
-
 		return self.email
-<<<<<<< HEAD
-class asfsdfdfsRegistrado(models.Model):
-=======
-class Registrado(models.Model):
->>>>>>> be5201d19cb32ff6e7c75d2913996fe0aedfd2c5
-	nombre = models.CharField(max_length=100,blank=True,null=True)
-	email=models.EmailField()
-	timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
