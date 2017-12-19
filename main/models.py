96fed2813616089f0cd016490827c90a0242a547
root <bryan12gh@gmail.com>
2017-12-19 12:42:52 -0500

    "Creando las urls de main"

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Registrado(models.Model):
-	nombre = models.CharField(max_length=100,blank=True,null=True)
+	nombre = models.CharField(max_length=110,blank=True,null=True)
 	email=models.EmailField()
 	timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
 	def __str__(self):
