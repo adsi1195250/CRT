af661560be285d6d206f9ce6c83c70dbab4356e8
Brayan Tabares <brayantabares10a2014@gmail.com>
2017-12-21 08:42:35 -0500

    Forms

--- main/forms.py
+++ main/forms.py
@@ -5,14 +5,80 @@ class trabajadoresForms(forms.ModelForm):
     class Meta:
         models = Trabajadores
         fields=[
-        'nombres',
-        'cedula',
-        'fechaIngreso',
-        'fechaNacimiento',
-        'edad',
-        'cargo',
-        'telefono',
-        'foto',
+            'nombres',
+            'cedula',
+            'fechaIngreso',
+            'fechaNacimiento',
+            'edad',
+            'cargo',
+            'telefono',
+            'foto',
         ]
+        labels = {
+            'nombres': 'NOMBRE COMPLETO',
+            'cedula': 'CEDULA',
+            'fechaIngreso': 'FECHA DE INGRESO',
+            'fechaNacimiento': 'FECHA DE NACIMIENTO',
+            'edad': 'EDAD',
+            'cargo': 'CARGO',
+            'telefono': 'TELEFONO',
+            'foto': 'FOTO',
+        }
 
+class CodigoBarrasForms(forms.ModelForm):
+    class Meta:
+        models = CodigoBarras
+        fields=[
+            'CodigoBarras',
+            'idTrabajadores',
+        ]
+        labels = {
+            'CodigoBarras': 'CODIGO BARRAS',
+            'idTrabajadores': 'ID  TRABAJADORES',
+        }
 
+class Historial_IOForms(forms.ModelForm):
+    class Meta:
+        models = Historial_IO
+        fields=[
+            'horaEntrada',
+            'horaSalida',
+            'horaDesayuno',
+            'horaDescanso',
+            'horaPausasActivas',
+            'horaAlmuerzo',
+        ]
+        labels = {
+            'horaEntrada': 'HORA DE ENTRADA',
+            'horaSalida': 'HORA DE SALIDA',
+            'horaDesayuno': 'HORA DE DESAYUNO',
+            'horaDescanso': 'HORA DE DESCANSO',
+            'horaPausasActivas': 'HORA DE PAUSAS ACTIVAS',
+            'horaAlmuerzo': 'HORA DE ALMUERZO',
+        }
+
+class PermisoAusentismoForms(forms.ModelForm):
+    class Meta:
+        models = PermisoAusentismo
+        fields=[
+            'fechaSalida',
+            #'totalHoras',
+            'motivo',
+            'periodoIncapacidadInicial',
+            'periodoIncapacidadFinal',
+            'diasIncapacidad',
+            'codigoDiagnostico',
+            'descipcion',
+            'idTrabajador',
+        ]
+        labels = {
+            'fechaSalida': 'FECHA DE SALIDA',
+            #'totalHoras': 'TOTAL DE HORAS',
+            'motivo': 'MOTIVO',
+            'periodoIncapacidadInicial': 'PERIODO DE INCAPACIDAD INICIAL',
+            'periodoIncapacidadFinal': 'PERIODO DE INCAPACIDAD FINAL',
+            'diasIncapacidad': 'DIAS DE INCAPACIDAD',
+            'codigoDiagnostico': 'CODIGO DE DIAGNOSTICO',
+            'descripcion': 'DESCRIPCION',
+            'idTrabajador': 'ID TRABAJADOR',
+        }
