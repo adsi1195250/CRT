7e51a580770ab82b7e3e797612b6034da2e9fbb4
Brayan Tabares <brayantabares10a2014@gmail.com>
2017-12-26 15:57:44 -0500

    views bitch

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DetailView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from main.forms import *
 from main.models import *
@@ -15,22 +15,29 @@ class ListarTrabajador(ListView):
     context_object_name = 'trabajador'
 
 class CrearTrabajador(CreateView):
+    model = Trabajadores
     template_name = 'Trabajadores/trabajador_modal.html'
     form_class = trabajadoresForms
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('listado_trabajadores')
 
 class ModificarTrabajador(UpdateView):
     model = Trabajadores
     form_class = trabajadoresForms
-    success_url = reverse_lazy('inicio')
+    template_name = 'Trabajadores/trabajador_modal.html'
+    success_url = reverse_lazy('listado_trabajadores')
 
 class DetalleTrabajador(DetailView):
     model = Trabajadores
     template_name = 'Trabajadores/detalle_trabajador.html'
 
+class EliminarTrabajador(DeleteView):
+    model = Trabajadores
+    template_name = 'Trabajadores/trabajador_eliminar.html'
+    success_url = reverse_lazy('listado_trabajadores')
+
 def registrarJornada(request):
-    return render(request, 'registrarJornada/registrarJornada.html')
+    return render(request, 'registrarJornada/registrarJornadaModal.html')
 
 class registrarJornadaModal(CreateView):
-    form_class = Historial_IOForms
+    model = Historial_IO
     template_name =  'registrarJornada/registrarJornadaModal.html'
